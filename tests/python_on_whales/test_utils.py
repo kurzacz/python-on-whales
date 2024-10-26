7f0100fca72d870182a68a45a00fcefb75d47d25
Kamil Kurzacz <kamil.kurzacz@gmail.com>
2024-10-26 22:12:29 +0200

    Add test for mapping Nones

--- tests/python_on_whales/test_utils.py
+++ tests/python_on_whales/test_utils.py
@@ -10,3 +10,8 @@ def test_environment_variables_propagation(monkeypatch):
         env={"OTHER_VARIABLE": "dudu"},
     )
     assert stdout == "dododada\ndudu"
+
+def test_format_mapping_for_cli_no_none():
+    mapping = {"test_env_var": None}
+    result = python_on_whales.utils.format_mapping_for_cli(mapping)
+    assert result == ["test_env_var="]
